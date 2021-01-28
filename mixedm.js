acb93ad2013d8717cf0945ed8f9abc0dc9bf8a1a
Gabor <vgabinfo@gmail.com>
2021-01-28 16:31:37 +0100

    create fantasyLife object and uniqueStory array

--- mixedm.js
+++ mixedm.js
@@ -2,5 +2,11 @@ function RandomNumber(num) {
     return Math.floor(Math.random() * num)
   }
 
+  const fantasyLife = {
+    planet: ['Jupiter', 'Saturn', 'Venus', 'Mars'],
+    destiny: ['king', 'priest', 'wizard', 'conqueror', 'servant'],
+    action: ['practice sword fighting', 'summon spirits', 'work tirelessly', 'meditate more']
+  }
   
+  let uniqueStory = []
  
